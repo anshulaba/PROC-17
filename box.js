679c019ee9b7d851287a64d1fefec39b2e90a1b6
anshulaba <72729339+anshulaba@users.noreply.github.com>
2022-02-26 16:53:49 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,21 @@
+class Box
+{
+    constructor(x,y,w,h,vx,vy)
+        {
+            this.x=x;
+            this.y=y;
+            this.w=w;
+            this.h=h;
+            this.vx=vx;
+            this.vy=vy;
+        }
+        show()
+        {
+            rect(this.x,this.y,this.w,this.h,this.vx,this.vy);
+        }
+        move()
+        {
+            this.y=this.y+this.vy;
+            this.x=this.x+this.vx;
+        }
+}
